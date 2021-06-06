58032ad10c61977b218a4ae897829aa627fba44d
pravdina <pravdina.tanya99@gmail.com>
2021-06-06 18:12:23 +0300

    функционал чекбоксов

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 var description_vis=true;
+
+function check(a){
+	if (document.getElementById(`${a}_checkbox`).checked) {
+        document
+        .getElementById(`${a}`)
+        .setAttribute("gltf-model", `#${a}_glb`); 
+    }
+    else {
+        document
+        .getElementById(`${a}`)
+        .removeAttribute("gltf-model");
+    }
+}
+
 window.onload = function () {
 	// Показать/спрятать теоретический текст
 	document
