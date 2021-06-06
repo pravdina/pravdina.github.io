1f9c448895d58b48f0a6d5780c85f39c9f6ee468
pravdina <pravdina.tanya99@gmail.com>
2021-06-06 18:42:40 +0300

    функционал радио кнопочек

--- main.js
+++ main.js
@@ -2,15 +2,53 @@ var description_vis=true;
 
 function check(a){
 	if (document.getElementById(`${a}_checkbox`).checked) {
-        document
-        .getElementById(`${a}`)
-        .setAttribute("gltf-model", `#${a}_glb`); 
-    }
-    else {
-        document
-        .getElementById(`${a}`)
-        .removeAttribute("gltf-model");
-    }
+		document
+		.getElementById(`${a}`)
+		.setAttribute("gltf-model", `#${a}_glb`); 
+	}
+	else {
+		document
+		.getElementById(`${a}`)
+		.removeAttribute("gltf-model");
+	}
+}
+
+function to_2d(){
+	// удаляем модель
+	document
+	.getElementById("model")
+	.removeAttribute("gltf-model");
+	// удаляем вспомогательные линии
+	document
+	.getElementById("lines")
+	.removeAttribute("gltf-model");
+	// очищаем анимейшен миксер
+	document
+	.getElementById("planes")
+	.removeAttribute("animation-mixer"); 
+	// назначаем новый анимейшен миксер
+	document
+	.getElementById("planes")
+	.setAttribute("animation-mixer", "clip: p*; timeScale: 1; clampWhenFinished: true; repetitions:1");
+}
+
+function to_3d(){
+	// делаем модель видимой
+	document
+	.getElementById("model")
+	.setAttribute("gltf-model", "#model_glb");
+	// делаем линии видимыми
+	document
+	.getElementById("lines")
+	.setAttribute("gltf-model", "#lines_glb");
+	// очищаем анимейшен миксер
+	document
+	.getElementById("planes")
+	.removeAttribute("animation-mixer"); 
+	// назначаем новый анимейшен миксер
+	document
+	.getElementById("planes")
+	.setAttribute("animation-mixer", "clip: p*; timeScale: -1; clampWhenFinished: true; repetitions:1");
 }
 
 window.onload = function () {
@@ -31,4 +69,7 @@ window.onload = function () {
 			description_vis=true;
 		}
 	});
+
+	// var rad=document.getElementsByName('options');
+	// alert(rad[0])
 }
