c6c80cf8dfc2c07594efdb36a5e742ad9cfeb42c
pravdina <pravdina.tanya99@gmail.com>
2021-06-07 17:33:30 +0300

    изменила порядок появления элементов+чтоб первой появлялась прямая общего положения

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ var straight_lines = [
 
 var description_vis=false;
 
-var current_straight_line = "gor_level";
+var current_straight_line = "zag_pol";
 var current_title = straight_lines.find(current_title => current_title.name === current_straight_line).title;
 var current_text = straight_lines.find(current_text => current_text.name === current_straight_line).text;
 
@@ -67,93 +67,93 @@ function check(a){
 function to_2d(){
 	// удаляем модель
 	document
-	.getElementById("model")
-	.removeAttribute("gltf-model");
+		.getElementById("model")
+		.removeAttribute("gltf-model");
 	// удаляем вспомогательные линии
 	document
-	.getElementById("lines")
-	.removeAttribute("gltf-model");
+		.getElementById("lines")
+		.removeAttribute("gltf-model");
 	// очищаем анимейшен миксер
 	document
-	.getElementById("planes")
-	.removeAttribute("animation-mixer"); 
+		.getElementById("planes")
+		.removeAttribute("animation-mixer"); 
 	// назначаем новый анимейшен миксер
 	document
-	.getElementById("planes")
-	.setAttribute("animation-mixer", "clip: p*; timeScale: 1; clampWhenFinished: true; repetitions:1");
+		.getElementById("planes")
+		.setAttribute("animation-mixer", "clip: p*; timeScale: 1; clampWhenFinished: true; repetitions:1");
 }
 
 function to_3d(){
-	// делаем модель видимой
-	document
-	.getElementById("model")
-	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
-	// делаем линии видимыми
-	document
-	.getElementById("lines")
-	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
 	// очищаем анимейшен миксер
 	document
-	.getElementById("planes")
-	.removeAttribute("animation-mixer"); 
+		.getElementById("planes")
+		.removeAttribute("animation-mixer"); 
 	// назначаем новый анимейшен миксер
 	document
-	.getElementById("planes")
-	.setAttribute("animation-mixer", "clip: p*; timeScale: -1; clampWhenFinished: true; repetitions:1");
+		.getElementById("planes")
+		.setAttribute("animation-mixer", "clip: p*; timeScale: -1; clampWhenFinished: true; repetitions:1");
+	// делаем модель видимой
+	document
+		.getElementById("model")
+		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
+	// делаем линии видимыми
+	document
+		.getElementById("lines")
+		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
 }
 
 window.onload = function () {
-	// console.log(current_title);
-	// console.log(current_text);
 	document.getElementById("title").innerHTML=(current_title);
 	document.getElementById("theory_text").innerHTML=(current_text);
-
+	// Сначала появляются плоскости, потом модель, потом линии
+	document
+		.getElementById("planes")
+		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
 	document
 		.getElementById("model")
 		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
 	document
 		.getElementById("lines")
 		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
-	document
-		.getElementById("planes")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
+	
 
 	// Показать/спрятать теоретический текст
 	document
-	.getElementById("info_btn")
-	.addEventListener("click", function () {
-		if(description_vis){
-			document
-			.getElementById("theory_text")
-			.setAttribute("style", "display: none;");
-			description_vis=false;
-		}
-		else{
-			document
-			.getElementById("theory_text")
-			.setAttribute("style", "display: block;");
-			description_vis=true;
-		}
+		.getElementById("info_btn")
+		.addEventListener("click", function () {
+			if(description_vis){
+				document
+				.getElementById("theory_text")
+				.setAttribute("style", "display: none;");
+				description_vis=false;
+			}
+			else{
+				document
+				.getElementById("theory_text")
+				.setAttribute("style", "display: block;");
+				description_vis=true;
+			}
 	});
 	// Изменение прямой
 	document
-	.getElementById('menu_optipns')
-	.addEventListener('click', function(e) {
+		.getElementById('menu_optipns')
+		.addEventListener('click', function(e) {
 	    if (e.target.tagName === 'LI'){
-	    	// Обновляем текущую прямую
+	// Обновляем текущую прямую
 	update_current(e.target.id);
+	// Меняем заголовок и описание
 	document.getElementById("title").innerHTML=(current_title);
 	document.getElementById("theory_text").innerHTML=(current_text);
-
+	// Сначала появляются плоскости, потом модель, потом линии
+	document
+		.getElementById("planes")
+		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
 	document
 		.getElementById("model")
 		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
 	document
 		.getElementById("lines")
 		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
-	document
-		.getElementById("planes")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
 	}
 	});
 }
