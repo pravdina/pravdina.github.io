d81971a3e8fcbf9da0f937e7d56128d7a83a31a9
pravdina <pravdina.tanya99@gmail.com>
2021-06-07 23:00:32 +0300

    доисправляем логические ошибки mode_3d = true; после функции складывания

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
 var path_before = "models/tema_1"
 
 var straight_lines = [
-	{
-		"name": "zag_pol",
-	    "title": "Пряма загального положення",
-	    "text": "Жодна з проекцій не паралельна осям.",
-	},
-	{
-		"name": "gor_level",
-	    "title": "Пряма горизонтального рівня",
-	    "text": "Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
-	},
-	{
-		"name": "front_level",
-	    "title": "Пряма фронтального рівня",
-	    "text": "Пряма фронтального рівня. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
-	},
-	{
-		"name": "prof_level",
-	    "title": "Пряма профільного рівня",
-	    "text": "Пряма профільного рівня. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
-	},
-	{
-		"name": "gor_proj",
-	    "title": "Горизонтально-проектуюча пряма",
-	    "text": "Горизонтально-проектуюча пряма. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
-	},
-	{
-		"name": "front_proj",
-	    "title": "Фронтально-проектуюча пряма",
-	    "text": "Фронтально-проектуюча пряма. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
-	},
-	{
-		"name": "prof_proj",
-	    "title": "Профільно-проектуюча пряма",
-	    "text": "Профільно-проектуюча пряма. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
-	}
-	]
+{
+	"name": "zag_pol",
+	"title": "Пряма загального положення",
+	"text": "Жодна з проекцій не паралельна осям.",
+},
+{
+	"name": "gor_level",
+	"title": "Пряма горизонтального рівня",
+	"text": "Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
+},
+{
+	"name": "front_level",
+	"title": "Пряма фронтального рівня",
+	"text": "Пряма фронтального рівня. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
+},
+{
+	"name": "prof_level",
+	"title": "Пряма профільного рівня",
+	"text": "Пряма профільного рівня. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
+},
+{
+	"name": "gor_proj",
+	"title": "Горизонтально-проектуюча пряма",
+	"text": "Горизонтально-проектуюча пряма. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
+},
+{
+	"name": "front_proj",
+	"title": "Фронтально-проектуюча пряма",
+	"text": "Фронтально-проектуюча пряма. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
+},
+{
+	"name": "prof_proj",
+	"title": "Профільно-проектуюча пряма",
+	"text": "Профільно-проектуюча пряма. Фронтальна та профільна проекції паралельні осям, горизонтальна проекція дорівнює самому відрізку.",
+}
+]
 
 var description_vis=false;
 var mode_3d = true;
@@ -70,29 +70,29 @@ function to_2d(){
 	if(mode_3d){
 		// удаляем модель
 		document
-			.getElementById("model")
-			.removeAttribute("gltf-model");
+		.getElementById("model")
+		.removeAttribute("gltf-model");
 		// удаляем вспомогательные линии
 		document
-			.getElementById("lines")
-			.removeAttribute("gltf-model");
+		.getElementById("lines")
+		.removeAttribute("gltf-model");
 		// очищаем анимейшен миксер
 		document
-			.getElementById("planes")
-			.removeAttribute("animation-mixer"); 
+		.getElementById("planes")
+		.removeAttribute("animation-mixer"); 
 		// назначаем новый анимейшен миксер
 		document
-			.getElementById("planes")
-			.setAttribute("animation-mixer", "clip: p*; timeScale: 1; clampWhenFinished: true; repetitions:1");
+		.getElementById("planes")
+		.setAttribute("animation-mixer", "clip: p*; timeScale: 1; clampWhenFinished: true; repetitions:1");
 
 		// Чекбоксы должны ВЫКЛЮЧИТЬСЯ (то есть если они нажаты, то осуществить имитацию нажатия)
-			if(document.getElementById("model_checkbox").checked){
-				document.getElementById("model_checkbox").click();
-			}
-			if(document.getElementById("lines_checkbox").checked){
-				document.getElementById("lines_checkbox").click();
-			}
-	mode_3d = false;
+		if(document.getElementById("model_checkbox").checked){
+			document.getElementById("model_checkbox").click();
+		}
+		if(document.getElementById("lines_checkbox").checked){
+			document.getElementById("lines_checkbox").click();
+		}
+		mode_3d = false;
 	}
 }
 
@@ -101,22 +101,22 @@ function to_3d(){
 	if(!mode_3d){
 		// очищаем анимейшен миксер
 		document
-			.getElementById("planes")
-			.removeAttribute("animation-mixer"); 
+		.getElementById("planes")
+		.removeAttribute("animation-mixer"); 
 		// назначаем новый анимейшен миксер
 		document
-			.getElementById("planes")
-			.setAttribute("animation-mixer", "clip: p*; timeScale: -1; clampWhenFinished: true; repetitions:1");
+		.getElementById("planes")
+		.setAttribute("animation-mixer", "clip: p*; timeScale: -1; clampWhenFinished: true; repetitions:1");
 
 		setTimeout(function() { 
 			// делаем модель видимой
 			document
-				.getElementById("model")
-				.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
+			.getElementById("model")
+			.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
 			// делаем линии видимыми
 			document
-				.getElementById("lines")
-				.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
+			.getElementById("lines")
+			.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
 
 			// Чекбоксы должны ВКЛЮЧИТЬСЯ (то есть если они НЕ нажаты, то осуществить имитацию нажатия)
 			if(!document.getElementById("model_checkbox").checked){
@@ -126,7 +126,8 @@ function to_3d(){
 				document.getElementById("lines_checkbox").click();
 			}
 
-			}, 5000);
+		}, 5000);
+		mode_3d = true;
 	}	
 	
 }
@@ -136,38 +137,38 @@ window.onload = function () {
 	document.getElementById("theory_text").innerHTML=(current_text);
 	// Сначала появляются плоскости, потом модель, потом линии
 	document
-		.getElementById("planes")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
+	.getElementById("planes")
+	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
 	document
-		.getElementById("model")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
+	.getElementById("model")
+	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
 	document
-		.getElementById("lines")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
+	.getElementById("lines")
+	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
 	
 
 	// Показать/спрятать теоретический текст
 	document
-		.getElementById("info_btn")
-		.addEventListener("click", function () {
-			if(description_vis){
-				document
-				.getElementById("theory_text")
-				.setAttribute("style", "display: none;");
-				description_vis=false;
-			}
-			else{
-				document
-				.getElementById("theory_text")
-				.setAttribute("style", "display: block;");
-				description_vis=true;
-			}
+	.getElementById("info_btn")
+	.addEventListener("click", function () {
+		if(description_vis){
+			document
+			.getElementById("theory_text")
+			.setAttribute("style", "display: none;");
+			description_vis=false;
+		}
+		else{
+			document
+			.getElementById("theory_text")
+			.setAttribute("style", "display: block;");
+			description_vis=true;
+		}
 	});
 	// Изменение прямой
 	document
-		.getElementById('menu_optipns')
-		.addEventListener('click', function(e) {
-	    if (e.target.tagName === 'LI'){
+	.getElementById('menu_optipns')
+	.addEventListener('click', function(e) {
+		if (e.target.tagName === 'LI'){
 	// Обновляем текущую прямую
 	update_current(e.target.id);
 	// Меняем заголовок и описание
@@ -175,14 +176,14 @@ window.onload = function () {
 	document.getElementById("theory_text").innerHTML=(current_text);
 	// Сначала появляются плоскости, потом модель, потом линии
 	document
-		.getElementById("planes")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
+	.getElementById("planes")
+	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/planes.glb`);
 	document
-		.getElementById("model")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
+	.getElementById("model")
+	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/model.glb`);
 	document
-		.getElementById("lines")
-		.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
-	}
-	});
+	.getElementById("lines")
+	.setAttribute("gltf-model", `${path_before}/${current_straight_line}/lines.glb`);
+}
+});
 }
