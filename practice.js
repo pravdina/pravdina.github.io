8e12b3e3103ee360e791bcbc5ff9ffa4db7ddbb0
pravdina <pravdina.tanya99@gmail.com>
2021-06-08 13:25:22 +0300

    возвращаем все

---
+++ practice.js
@@ -0,0 +1,9 @@
+var path_before = "models/tema_1";
+
+window.onload = function () {
+	document
+		.getElementById("main_btn")
+		.addEventListener("click", function () {
+			alert("fhfhf");
+	});
+}
