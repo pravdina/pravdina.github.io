ddd70f0cb512c690d0818f0ac34b9f2983ff6ac4
pravdina <pravdina.tanya99@gmail.com>
2021-10-03 11:55:42 +0300

    Revert "back to normal"
    This reverts commit 6607abb2a87da6d5ad3e5d80ed46c7fc9f07ccaf.

--- tema_2.js
+++ tema_2.js
@@ -203,4 +203,7 @@ window.onload = () => {
   document
     .getElementById('menu_options')
     .addEventListener('click', (e) => handleLineChange(e));
+  // Радиобатоны
+  document.getElementById('to_3d_radio').onclick = to3D();
+  document.getElementById('to_2d_radio').onclick = to2D();
 };
